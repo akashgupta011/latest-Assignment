74c5f9e20cd4003634b79a1d43887a2ab1c4a567
Akash <akashgupta@blubirch.com>
2023-12-20 17:21:24 +0530

    issue resolve

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@
 
 class Post < ApplicationRecord
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
   
   # Associations
   belongs_to :user
@@ -22,7 +22,7 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   # Callbacks
-  after_save :process_images
+  # after_save :process_images
 
   # Validations
   validates :title, presence: true
@@ -37,34 +37,45 @@ class Post < ApplicationRecord
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
 
-  mapping do
-    indexes :title, type: 'text'
-    indexes :content, type: 'text'
-    # Add more indexes for categories and tags as needed
+  # mapping do
+  #   indexes :title, type: 'text'
+  #   indexes :content, type: 'text'
+  #   # Add more indexes for categories and tags as needed
+  # end
+
+  # def as_indexed_json(_options = {})
+  #   as_json(only: %w[title content])
+  # end
+
+  # # Ensure the Elasticsearch index exists before importing
+  # Post.__elasticsearch__.create_index! force: true
+
+  # # Index the existing data after the index is created
+  # Post.import
+
+  require 'csv'
+  def self.to_csv
+    posts = Post.all
+    CSV.generate do |csv|
+      csv << column_names
+      posts.each do |post|
+        csv << post.attributes.values_at(*column_names)
+      end
+    end
   end
-
-  def as_indexed_json(_options = {})
-    as_json(only: %w[title content])
-  end
-
-  # Ensure the Elasticsearch index exists before importing
-  Post.__elasticsearch__.create_index! force: true
-
-  # Index the existing data after the index is created
-  Post.import
 
   private
 
   # Private: Process images after saving the post.
   #
-  # Returns nothing.
-  def process_images
-    return unless image.attached?
+  # # Returns nothing.
+  # def process_images
+  #   return unless image.attached?
 
-    # Create a thumbnail (100x100)
-    image.variant(resize: '100x100').processed
+  #   # Create a thumbnail (100x100)
+  #   image.variant(resize: '100x100').processed
 
-    # Create a preview (300x300)
-    image.variant(resize: '300x300').processed
-  end
+  #   # Create a preview (300x300)
+  #   image.variant(resize: '300x300').processed
+  # end
 end
